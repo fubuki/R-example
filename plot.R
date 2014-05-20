5e106d8c160d42c208c4c7734dbf668d610b6299
fubuki <fubuki@users.noreply.github.com>
2014-05-20 22:22:14 +0800

    Update plot.R

--- plot.R
+++ plot.R
@@ -6,3 +6,17 @@ par(mfrow=c(2,2)) # Create a 2x2 plotting area
 plot(x, dunif(x,min=2,max=4), main="Uniform", type='l', ylim=ylim)
 plot(x, dnorm(x,mean=3,sd=1), main="Normal", type='l', ylim=ylim) 
 plot(x, dgamma(x,shape=2,rate=1), main="Gamma", type='l', ylim=ylim)
+
+x <- seq(from=-3, to=+3, length.out=100)
+y <- dnorm(x)
+plot(x, y, main="Standard Normal Distribution", type='l', ylab="Density", xlab="Quantile")
+abline(h=0)
+
+region.x <- x[1 <= x & x <= 2]
+region.y <- y[1 <= x & x <= 2]
+
+polygon(region.x, region.y, density=-1, col="red")
+
+
+
+
