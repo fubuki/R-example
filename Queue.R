f444d52234dc39c79e3f31b91f7a6ec5bea5879d
fubuki <fubuki@users.noreply.github.com>
2014-06-11 00:13:14 +0800

    Update Queue.R

--- Queue.R
+++ Queue.R
@@ -1,3 +1,7 @@
+library(ineq)
+library(pdq)
+
+
 t.end   <- 10^5 # duration of sim
 t.clock <- 0    # sim time
 Ta <- 1.3333    # interarrival period
@@ -18,36 +22,43 @@ set.seed(1)
 
 
 while (t.clock < t.end) {
-    if (t1 < t2) {      # arrival event
-        t.clock <- t1
-        s <- s + n * (t.clock - tn)  # delta time-weighted number in queue
-        n <- n + 1
-        if (t.clock < plotSamples) { 
-            qc <- append(qc,n)
-            tc <- append(tc,t.clock) 
-        }
-        tn <- t.clock
-        t1 <- t.clock + rexp(1, 1/Ta)
-        if(n == 1) { 
-            tb <- t.clock
-            t2 <- t.clock + rexp(1, 1/Ts)  # exponential  interarrival period
-        }
-    } else {            # departure event
-        t.clock <- t2
-        s <- s + n * (t.clock - tn)  # delta time-weighted number in queue
-        n <- n - 1
-        if (t.clock < plotSamples) { 
-            qc <- append(qc,n)
-            tc <- append(tc,t.clock)
-        }
-        tn <- t.clock
-        c <- c + 1
-        if (n > 0) { 
-            t2 <- t.clock + rexp(1, 1/Ts)  # exponential  service period
-        }
-        else { 
-            t2 <- t.end
-            b <- b + t.clock - tb
-        }
-    }   
+  if (t1 < t2) {      # arrival event
+    t.clock <- t1
+    s <- s + n * (t.clock - tn)  # delta time-weighted number in queue
+    n <- n + 1
+    if (t.clock < plotSamples) { 
+      qc <- append(qc,n)
+      tc <- append(tc,t.clock) 
+    }
+    tn <- t.clock
+    t1 <- t.clock + rexp(1, 1/Ta)
+    if(n == 1) { 
+      tb <- t.clock
+      t2 <- t.clock + rexp(1, 1/Ts)  # exponential  interarrival period
+    }
+  } else {            # departure event
+    t.clock <- t2
+    s <- s + n * (t.clock - tn)  # delta time-weighted number in queue
+    n <- n - 1
+    if (t.clock < plotSamples) { 
+      qc <- append(qc,n)
+      tc <- append(tc,t.clock)
+    }
+    tn <- t.clock
+    c <- c + 1
+    if (n > 0) { 
+      t2 <- t.clock + rexp(1, 1/Ts)  # exponential  service period
+    }
+    else { 
+      t2 <- t.end
+      b <- b + t.clock - tb
+    }
+  }   
 }
+
+
+u <- b/t.clock       # utilization B/T
+N <- s/t.clock       # mean queue length (see the Load Average notes)
+x <- c/t.clock       # mean throughput C/T
+r <- N/x             # mean residence time (from Little's law: Q = XR)
+q <- sum(qc)/max(tc) # estimated queue length for plot
