4c3c8328b55eda76c0bc910c509e73961dfab969
fubuki <fubuki@users.noreply.github.com>
2014-07-02 00:28:48 +0800

    Update FSA.R

--- FSA.R
+++ FSA.R
@@ -1,1 +1,9 @@
-R
+library(tm)
+ 
+# Load the tweets
+tweets <- unique(read.table('data/tweets_25bahman.csv', sep="\t", quote="", comment.char="", header=FALSE, nrows=100000, stringsAsFactors=FALSE))
+names(tweets) <- c("id", "date", "user", "text")
+ 
+# Build the corpus and then apply the tm pre-processing methods
+corpus <- Corpus(VectorSource(tweets$text))
+corpus <- tm_map(tm_map(tm_map(corpus, stripWhitespace), tolower), stemDocument)
